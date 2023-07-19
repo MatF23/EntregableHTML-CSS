89f2cf02324eef0c3ac2e1d20db2a0713d96fb88
MatF23 <elmat-f@hotmail.com>
2023-07-18 21:44:37 -0300

    Se agrego funcion js

---
+++ app.js
@@ -0,0 +1,47 @@
+// Codigo para probar que haya quedado enlazado el documento al html
+// document.getElementById("submitbtn").addEventListener('click', ()=> {window.alert('Hola!')})
+
+
+
+function submitHandler() {
+
+// const formElem = document.getElementsByTagName("form")[0];
+// formElem.addEventListener("submit", (event) => {
+// event.preventDefault();
+// }) para anular la configuracion predeterminada del form, que es enviar al back y refrescar pantalla
+
+const name = document.getElementById('fName')
+const lName = document.getElementById('lName')
+const emailA = document.getElementById('emailAdress')
+const companyName = document.getElementById('companyName')
+const phoneNumber = document.getElementById('phoneNumber')
+const message = document.getElementById('message')
+
+const nameV = name.value
+const lNameV = lName.value
+const emailV = emailA.value
+const companyV = companyName.value
+const phoneV = phoneNumber.value
+const messV = message.value
+
+function addAListener() {
+    const submitbtnI = document.getElementById("submitbtn")
+    submitbtnI.addEventListener('click', alertMessage)
+}
+addAListener()
+
+
+if (nameV && lNameV && emailV && companyV && phoneV && messV) { 
+    alertMessage()
+}
+
+function alertMessage() {
+    console.log(`First Name: ${nameV}, Last Name: ${lNameV}, Email Adress: ${emailV}, Company Name: ${companyV}, Phone Number: ${phoneV}, Message: ${messV}`)
+    window.alert(`First Name: ${nameV}, Last Name: ${lNameV}, Email Adress: ${emailV}, Company Name: ${companyV}, Phone Number: ${phoneV}, Message: ${messV}`)
+}
+
+}
+
+// submitHandler() pense que habia que llamar la funcion para que se ejecute,
+// pero si lo hago se ejecuta dos veces, una por el onclick en el html y otra
+// por llamarla aca
